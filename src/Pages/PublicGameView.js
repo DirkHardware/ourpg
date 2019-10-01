7ee030677dc0100edbd7a6416a89d25b59d9a567
Flatiron School <email.the.anderson@gmail.com>
2019-10-01 17:02:18 -0400

    further styling

--- src/Pages/PublicGameView.js
+++ src/Pages/PublicGameView.js
@@ -3,12 +3,35 @@ import { connect } from 'react-redux'
 import { fetchGameElements } from '../actions'
 import '../index.css';
 import PublicElementCard from '../Cards/PublicElementCard'
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 class PublicGameView extends React.Component {
 
     componentDidMount() {
         // fire off dispatch to send my fetch request 
         this.props.fetchGameElements()
+    }
+
+    print = elements => {
+        let i;
+        let contentArray = []
+        for(i = 0; i < elements.length; i++){
+          contentArray.push({
+            text: elements[i].title,
+            style: 'header'
+          })
+          contentArray.push('        ')
+          contentArray.push(elements[i].content)
+          contentArray.push('        ')
+        };
+        let docDefinition = {content: contentArray, styles: { header: {
+          fontSize: 18,
+          bold: true
+        }}};
+        console.log(docDefinition)
+        pdfMake.createPdf(docDefinition).download();
     }
 
     render() {
@@ -27,6 +50,7 @@ class PublicGameView extends React.Component {
             />)
         return (
             <div className='public-elements-container'>
+                 <button onClick={() => this.print(this.props.gameElements)}>Export</button>
                 {elementComponents}
             </div>
         )
